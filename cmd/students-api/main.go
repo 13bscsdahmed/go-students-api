96148c3de085e41fc467006b7af3940d2ba921f7
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2026-09-10 12:47:37 +0500

    Graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"students-api/internal/config"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg := config.MustLoad()
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Students API"))
+	})
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Address,
+		Handler: router,
+	}
+	slog.Info("Server Started", slog.String("address", cfg.HTTPServer.Address))
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server: ")
+		}
+	}()
+	<-done
+
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	error := server.Shutdown(ctx)
+	if error != nil {
+		slog.Error("Failed to shutdown the server", slog.String("error", error.Error()))
+	}
+	slog.Info("Server shutdown successfully")
 }
